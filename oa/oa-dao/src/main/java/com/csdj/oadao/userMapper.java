fc12c0ca174069aca0c785486464618f87aa0e30
fyq <1524503229@qq.com>
2020-04-08 16:22:54 +0800

    删除oa

--- oa/oa-dao/src/main/java/com/csdj/oadao/userMapper.java
+++
@@ -1,9 +0,0 @@
-package com.csdj.oadao;
-
-import com.csdj.pojo.Users;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface userMapper extends tk.mybatis.mapper.common.Mapper<Users> {
-
-}
--- oa/oa-service/src/main/java/com/csdj/service/userService.java
+++
@@ -1,9 +0,0 @@
-package com.csdj.service;
-
-import com.csdj.pojo.Users;
-
-import java.util.List;
-
-public interface userService {
-    public List<Users> getAll();
-}
